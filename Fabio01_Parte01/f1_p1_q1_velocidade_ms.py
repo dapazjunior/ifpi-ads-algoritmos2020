6b8d54f666bc5794c017f1248808c63391188936
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-25 13:01:37 -0300

    Update f1_p1_q1_velocidade_ms.py

--- Fabio01_Parte01/f1_p1_q1_velocidade_ms.py
+++ Fabio01_Parte01/f1_p1_q1_velocidade_ms.py
@@ -1,8 +1,16 @@
 # Entrada
-velocidade_ms = float(input('Digite velocidade em m/s: '))
+#Leia uma velocidade em m/s, calcule e escreva esta velocidade em km/h. (Vkm/h = Vm/s * 3.6)
 
-# Processamento
-velocidade_kmh = velocidade_ms * 3.6
+def main():
+    velocidade_ms = float(input('Digite velocidade em m/s: '))
 
-# Saída
-print(f'A velocidade é {velocidade_kmh:.2f} km/h ')
+    velocidade_kmh = calcular_vel_kmh(velocidade_ms)
+
+    print(f'A velocidade é {velocidade_kmh:.1f} km/h ')
+
+
+def calcular_vel_kmh(velocidade_ms):
+    velocidade_kmh = velocidade_ms * 3.6
+    return velocidade_kmh
+
+main()
