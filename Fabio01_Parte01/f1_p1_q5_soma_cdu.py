5381adabdb49bc690a8d54f0c8f1b214922785a5
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-25 17:06:00 -0300

    Update f1_p1_q5_soma_cdu.py

--- Fabio01_Parte01/f1_p1_q5_soma_cdu.py
+++ Fabio01_Parte01/f1_p1_q5_soma_cdu.py
@@ -1,11 +1,33 @@
-# Entrada
-numero = int(input('Digite um numero inteiro de 3 algarismos: '))
+def main():
+    numero = int(input('Digite um numero inteiro de 3 algarismos: '))
+    
+    soma = soma_algs(numero)
+    
+    print(f'A soma dos algarismos é {soma}.')
 
-# Processamento
-centena = numero // 100
-dezena = (numero % 100) // 10
-unidade = (numero % 100) % 10
-soma = centena + dezena + unidade
 
-# Saida
-print(f'A soma dos algarismos é {soma}.')
+def alg_centena(numero):
+    centena = numero // 100
+    return centena
+
+
+def alg_dezena(numero):
+    dezena = (numero % 100) // 10
+    return dezena
+
+
+def alg_unidade(numero):
+    unidade = (numero % 100) % 10
+    return unidade
+
+def soma_algs(numero):
+    c = alg_centena(numero)
+    d = alg_dezena(numero)
+    u = alg_unidade(numero)
+    
+    soma = c + d + u
+    
+    return soma
+
+
+main()
