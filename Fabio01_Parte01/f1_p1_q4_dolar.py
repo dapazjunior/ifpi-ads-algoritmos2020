83b03a4b64fde22b7a8a0e15b7badb43cf6b2da3
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-25 16:52:19 -0300

    Update f1_p1_q4_dolar.py

--- Fabio01_Parte01/f1_p1_q4_dolar.py
+++ Fabio01_Parte01/f1_p1_q4_dolar.py
@@ -1,10 +1,21 @@
-# Entrada
-dolar1 = float(input('Digite um valor em dólar: '))
-dolar2 = float(input('Digite outro valor em dólar: '))
+def main():
+    dolar1 = float(input('Digite um valor em dólar: '))
+    dolar2 = float(input('Digite outro valor em dólar: '))
 
-# Processamento
-dolar_total = dolar1 + dolar2
-real = dolar_total * 4.37
+    dolar_total = somar_dolar(dolar1, dolar2)
+    real = conversor(dolar_total)
 
-# Saída
-print(f'O valor é R$ {real:.2f}')
+    print(f'O valor é R$ {real:.2f}')
+
+
+def somar_dolar(dolar1, dolar2):
+    dolar_total = dolar1 + dolar2
+    return dolar_total
+
+
+def conversor(dolar_total):
+    real = dolar_total * 4.98
+    return real
+
+
+main()
