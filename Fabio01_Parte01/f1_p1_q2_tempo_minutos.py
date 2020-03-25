0f4caf100c8559e75c69c4b0631b0765a5bdfbfc
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-25 15:03:04 -0300

    Update f1_p1_q2_tempo_minutos.py

--- Fabio01_Parte01/f1_p1_q2_tempo_minutos.py
+++ Fabio01_Parte01/f1_p1_q2_tempo_minutos.py
@@ -1,9 +1,23 @@
-# Entrada
-horas = int(input('Digite um valor em horas: '))
-minutos_parcial = int(input('Digite um valor em minutos: '))
+def main():
+    '''Lê um valor em horas e um valor em minutos.
+    O valor em horas é convertido em minutos e depois somado com o valor em minutos fornecido.
+    
+    Retorna o valor total em minutos'''
+    
+    horas = int(input('Digite um valor em horas: '))
+    minutos_parcial = int(input('Digite um valor em minutos: '))
+    
+    minutos_total = calcular_minutos(horas, minutos_parcial)
+    
+    print(f'O valor é {minutos_total} minutos')
 
-# Processamento
-minutos_total = horas * 60 + minutos_parcial
 
-# Saída
-print(f'O valor é {minutos_total} minutos')
+def calcular_minutos(horas, minutos_parcial):
+    h_em_min = horas * 60
+    
+    minutos_total = h_em_min + minutos_parcial
+    
+    return minutos_total
+
+
+main()
