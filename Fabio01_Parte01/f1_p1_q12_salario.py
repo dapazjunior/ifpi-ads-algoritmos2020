359a639d88ec2e5046be6e01ad16fb45d707146a
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:54:50 -0300

    Delete f1_p1_q12_salario.py

--- Fabio01_Parte01/f1_p1_q12_salario.py
+++
@@ -1,8 +0,0 @@
-# Entrada
-salario = float(input('Digite o seu salário atual: R$ '))
-
-# Processamento
-novo_salario = salario * 1.25
-
-# Saida
-print(f'O seu novo salário é R$ {novo_salario:.2f}.')
