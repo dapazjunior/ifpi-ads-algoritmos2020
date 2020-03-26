1aa9b73be4e679c2bd819fbfb83fb5ae49aa2807
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:55:27 -0300

    Delete f1_p1_q18_comprimento_circunferencia.py

--- Fabio01_Parte01/f1_p1_q18_comprimento_circunferencia.py
+++
@@ -1,9 +0,0 @@
-# Entrada
-raio = float(input('Digite a medida do raio da circunferência: '))
-
-# Processamento
-pi = 3.1415
-comprimento = 2 * pi * raio
-
-# Saida
-print(f'O comprimento da circunferência é {comprimento:.2f}.')
