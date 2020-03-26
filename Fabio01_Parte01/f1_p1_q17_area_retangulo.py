71aa6b46822dd73e1cc41a747a1cad3df60ca92e
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:55:04 -0300

    Delete f1_p1_q17_area_retangulo.py

--- Fabio01_Parte01/f1_p1_q17_area_retangulo.py
+++
@@ -1,9 +0,0 @@
-# Entrada
-base = float(input('Digite a medida da base do retângulo: '))
-altura = float(input('Digite a medida da altura do retângulo: '))
-
-# Processamento
-area = base * altura
-
-# Saida
-print(f'A área do retângulo é {area:.2f}.')
