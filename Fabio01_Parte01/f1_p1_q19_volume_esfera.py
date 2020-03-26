be5eb9e2dcadf8e719b46e44ef731d60c770fe26
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:55:09 -0300

    Delete f1_p1_q19_volume_esfera.py

--- Fabio01_Parte01/f1_p1_q19_volume_esfera.py
+++
@@ -1,9 +0,0 @@
-# Entrada
-raio = float(input('Digite a medida do raio da esfera: '))
-
-# Processamento
-pi = 3.1415
-volume = (4 * pi * (raio ** 3))/3
-
-# Saida
-print(f'O volume da esfera é {volume:.2f}.')
