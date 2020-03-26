288fdf446c85951559bcef7a90cb01a64a5371f5
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:57:14 -0300

    Delete f1_p2_q6_anos_meses.py

--- Fabio01_Parte02/f1_p2_q6_anos_meses.py
+++
@@ -1,9 +0,0 @@
-# Entrada
-meses_totais = int(input('Digite um valor inteiro de meses: '))
-
-# Processamento
-anos = meses_totais // 12
-meses_resto = meses_totais % 12
-
-# Saida
-print (f'O valor é {anos} anos e {meses_resto} meses')
