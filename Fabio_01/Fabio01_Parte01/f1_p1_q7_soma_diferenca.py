96165effacb46b870b451bb5cb444e11e4ed5a90
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 16:41:38 -0300

    Update f1_p1_q7_soma_diferenca.py

--- Fabio_01/Fabio01_Parte01/f1_p1_q7_soma_diferenca.py
+++ Fabio_01/Fabio01_Parte01/f1_p1_q7_soma_diferenca.py
@@ -1,11 +1,23 @@
-# Entrada
-numero_1 = float(input('Digite um número: '))
-numero_2 = float(input('Digite outro número: '))
-numero_3 = float(input('Digite outro número: '))
+def main():
+    num1 = float(input('Digite um número: '))
+    num2 = float(input('Digite outro número: '))
+    num3 = float(input('Digite outro número: '))
+    
+    soma_1_e_2 = calc_soma_1_e_2(num1, num2)
+    
+    diferenca_2_e_3 = calc_diferenca_2_e_3(num2, num3)
+    
+    print (f'A soma entre {num1} e {num2} é {soma_1_e_2}, e a diferença entre {num2} e {num3} é {diferenca_2_e_3}.')
 
-# Processamento
-soma = numero_1 + numero_2
-diferenca = numero_2 - numero_3
 
-# Saida
-print (f'A soma entre {numero_1} e {numero_2} é {soma}, e a diferença entre {numero_2} e {numero_3} é {diferenca}.')
+def calc_soma_1_e_2(numero_1, numero_2):
+    soma = numero_1 + numero_2
+    return soma
+
+
+def calc_diferenca_2_e_3(numero_2, numero_3):
+    diferenca = numero_2 - numero_3
+    return diferenca
+
+
+main()
