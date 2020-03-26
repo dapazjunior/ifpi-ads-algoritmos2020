368a9abe643cce8c53a7723475290cdeda672eed
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:57:19 -0300

    Delete f1_p2_q23_prestacoes.py

--- Fabio01_Parte02/f1_p2_q23_prestacoes.py
+++
@@ -1,9 +0,0 @@
-# Entrada
-valor = float(input('Qual o valor do produto? '))
-
-# Processamento
-prestacao = valor // 3
-entrada = (valor // 3) + (valor % 3)
-
-# Saída
-print(f'O pagamento será uma entrada de R$ {entrada:.2f} e mais duas prestações de R$ {prestacao:.2f}.')
