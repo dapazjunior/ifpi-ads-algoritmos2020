d995fefac50ad62c687045e084fb8801eea0a8cb
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:56:41 -0300

    Delete f1_p1_q9_numero_ab_ba.py

--- Fabio01_Parte01/f1_p1_q9_numero_ab_ba.py
+++
@@ -1,8 +0,0 @@
-# Entrada
-numero_1 = int(input('Digite um número: '))
-numero_2 = int(input('Digite outro número: '))
-
-# Processamento
-
-# Saida
-print(f'Você digitou {numero_1, numero_2}, o resultado é {numero_2, numero_1}.')
