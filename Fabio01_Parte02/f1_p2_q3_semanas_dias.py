8133b4a33e52e238b6441e846e7d9f38a43db67e
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-26 15:58:26 -0300

    Delete f1_p2_q3_semanas_dias.py

--- Fabio01_Parte02/f1_p2_q3_semanas_dias.py
+++
@@ -1,9 +0,0 @@
-# Entrada
-dias_totais = int(input('Digite um valor inteiro de dias: '))
-
-# Processamento
-semana = dias_totais // 7
-dias_resto = dias_totais % 7
-
-# Saida
-print (f'O valor é {semana} semanas e {dias_resto} dias')
