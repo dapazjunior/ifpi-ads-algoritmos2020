74d74897251fa85a6c447e1a851db2cfe0fe3034
dapazjunior <dapazjunior@users.noreply.github.com>
2020-03-28 15:55:32 -0300

    Update f2_a_q22_duracao_jogo.py

--- Fabio_02/Fabio02_Parte_a/f2_a_q22_duracao_jogo.py
+++ Fabio_02/Fabio02_Parte_a/f2_a_q22_duracao_jogo.py
@@ -5,8 +5,10 @@ def main():
     horas_fim = int(input('Qual o horário de início do jogo? (HH:MM)\nHoras:'))
     minutos_fim = int(input('Minutos: '))
     
-    print('A partida durou ', end='')
-    calcular_duracao(horas_inicio, horas_fim, minutos_inicio, minutos_fim)
+    
+    duracao_jogo = calcular_duracao(horas_inicio, horas_fim, minutos_inicio, minutos_fim)
+    
+    print(f'A partida durou {duracao_jogo}')
     
     
 def calcular_duracao(horas_inicio, horas_fim, minutos_inicio, minutos_fim):
@@ -16,10 +18,10 @@ def calcular_duracao(horas_inicio, horas_fim, minutos_inicio, minutos_fim):
     
     if tempo_fim < tempo_inicio:
         duracao = 24 * 60 - tempo_inicio + tempo_fim
-        return print(f'{duracao // 60}:{duracao % 60}.')
+        return f'{duracao // 60}:{duracao % 60}.'
     elif tempo_inicio < tempo_fim:
         duracao = tempo_fim - tempo_inicio
-        return print(f'{duracao // 60}:{duracao % 60}.')
+        return f'{duracao // 60}:{duracao % 60}.'
 
 
 main()
