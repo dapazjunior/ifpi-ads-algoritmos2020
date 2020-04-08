6ed35a8ca2415df06a591a307787bff535603991
dapazjunior <dapazjunior@users.noreply.github.com>
2020-04-08 11:58:32 -0300

    Create q1010.py

---
+++ URI/q1010.py
@@ -0,0 +1,13 @@
+codigo1, num1, valor1 = input().split(' ')
+codigo2, num2, valor2 = input().split(' ')
+
+codigo1 = int(codigo1)
+codigo2 = int(codigo2)
+num1 = int(num1)
+num2 = int(num2)
+valor1 = float(valor1)
+valor2 = float(valor2)
+
+total = num1 * valor1 + num2 * valor2
+
+print('VALOR A PAGAR: R$ {:.2f}'.format(total))
