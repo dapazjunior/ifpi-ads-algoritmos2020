b1b9908ab3bc78682a3bca68a0b86b111f0fdf0e
dapazjunior <dapazjunior@users.noreply.github.com>
2020-04-17 15:09:49 -0300

    Update q1010.py

--- URI/q1010.py
+++ URI/q1010.py
@@ -1,13 +1,19 @@
-codigo1, num1, valor1 = input().split(' ')
-codigo2, num2, valor2 = input().split(' ')
+def main():
+    codigo1, num1, valor1 = input().split(' ')
+    codigo2, num2, valor2 = input().split(' ')
 
-codigo1 = int(codigo1)
-codigo2 = int(codigo2)
-num1 = int(num1)
-num2 = int(num2)
-valor1 = float(valor1)
-valor2 = float(valor2)
+    codigo1 = int(codigo1)
+    codigo2 = int(codigo2)
+    
+    num1 = int(num1)
+    num2 = int(num2)
+    
+    valor1 = float(valor1)
+    valor2 = float(valor2)
 
-total = num1 * valor1 + num2 * valor2
+    total = num1 * valor1 + num2 * valor2
 
-print('VALOR A PAGAR: R$ {:.2f}'.format(total))
+    print('VALOR A PAGAR: R$ {:.2f}'.format(total))
+
+
+main()
