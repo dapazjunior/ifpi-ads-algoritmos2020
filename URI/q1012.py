c2efbff7114cfe3a6968fd76bac9f1b3dc8d96df
dapazjunior <dapazjunior@users.noreply.github.com>
2020-04-17 15:09:39 -0300

    Update q1012.py

--- URI/q1012.py
+++ URI/q1012.py
@@ -1,20 +1,24 @@
-a, b, c = input().split()
-a = float(a)
-b = float(b)
-c = float(c)
+def main():
+    a, b, c = input().split()
+    a = float(a)
+    b = float(b)
+    c = float(c)
 
-triangulo = a * c / 2
-print('TRIANGULO: {:.3f}'.format(triangulo))
+    triangulo = a * c / 2
+    print('TRIANGULO: {:.3f}'.format(triangulo))
 
-pi = 3.14159
-circulo = pi * c**2
-print('CIRCULO: {:.3f}'.format(circulo))
+    pi = 3.14159
+    circulo = pi * c**2
+    print('CIRCULO: {:.3f}'.format(circulo))
 
-trapezio = (a + b) * c / 2
-print('TRAPEZIO: {:.3f}'.format(trapezio))
+    trapezio = (a + b) * c / 2
+    print('TRAPEZIO: {:.3f}'.format(trapezio))
 
-quadrado = b**2
-print('QUADRADO: {:.3f}'.format(quadrado))
+    quadrado = b**2
+    print('QUADRADO: {:.3f}'.format(quadrado))
 
-retangulo = a * b
-print('RETANGULO: {:.3f}'.format(retangulo))
+    retangulo = a * b
+    print('RETANGULO: {:.3f}'.format(retangulo))
+
+
+main()
