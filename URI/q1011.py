b0c5064cb458454f4d042f0e3e50b0310e501b5a
dapazjunior <dapazjunior@users.noreply.github.com>
2020-04-17 15:09:44 -0300

    Update q1011.py

--- URI/q1011.py
+++ URI/q1011.py
@@ -1,6 +1,10 @@
-raio = float(input())
-pi = 3.14159
+def main():
+    raio = float(input())
+    pi = 3.14159
 
-volume = 4/3 * pi * raio**3
+    volume = 4/3 * pi * raio**3
 
-print('VOLUME = {:.3f}'.format(volume))
+    print('VOLUME = {:.3f}'.format(volume))
+
+
+main()
