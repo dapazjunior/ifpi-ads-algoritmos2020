710afb5620127a6421a09e065a44d3e407341142
dapazjunior <dapazjunior@users.noreply.github.com>
2020-04-17 15:10:12 -0300

    Update q1005.py

--- URI/q1005.py
+++ URI/q1005.py
@@ -1,4 +1,10 @@
-nota_a = float(input())
-nota_b = float(input())
-media = (nota_a*3.5 + nota_b*7.5) / 11
-print('MEDIA = {:.5f}'.format(media))
+def main():
+    nota_a = float(input())
+    nota_b = float(input())
+    
+    media = (nota_a*3.5 + nota_b*7.5) / 11
+    
+    print('MEDIA = {:.5f}'.format(media))
+
+
+main()
