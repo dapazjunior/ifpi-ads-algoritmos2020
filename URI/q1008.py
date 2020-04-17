ad0c4011b54139faa5aefa7dfc817f06d72adb0a
dapazjunior <dapazjunior@users.noreply.github.com>
2020-04-17 15:09:58 -0300

    Update q1008.py

--- URI/q1008.py
+++ URI/q1008.py
@@ -1,7 +1,12 @@
-num = int(input())
-horas = int(input())
-valor_hora = float(input())
-salario = horas * valor_hora
+def main():
+    num = int(input())
+    horas = int(input())
+    valor_hora = float(input())
+    
+    salario = horas * valor_hora
 
-print('NUMBER = {}'.format(num))
-print('SALARY = U$ {:.2f}'.format(salario))
+    print('NUMBER = {}'.format(num))
+    print('SALARY = U$ {:.2f}'.format(salario))
+
+
+main()
